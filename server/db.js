723f7b0f5db45bae4875cf3d125424582362e59e
Alvin Albert Michael <113367440+alvin-dotcom@users.noreply.github.com>
2024-10-20 13:29:28 +0530

    commit
    1st commit

---
+++ server/db.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+pool.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database', err);
+  } else {
+    console.log('Connected to the PostgreSQL database');
+  }
+});
+
+module.exports = pool;
