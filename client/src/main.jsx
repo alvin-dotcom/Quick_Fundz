a56c663a79fa07785842bc0cf05e826a24493c66
Rishav Raj <rajrishav011@gmail.com>
2024-10-22 01:26:37 +0530

    Add main.jsx file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./redux/store.jsx";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HelmetProvider>
+      <ReduxProvider store={store}>
+          <BrowserRouter>
+              <App />
+              <ToastContainer
+              position="bottom-center"
+              autoClose={3000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              theme="dark"
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              />
+          </BrowserRouter>
+          </ReduxProvider>
+    </HelmetProvider>,
   </StrictMode>,
 )
