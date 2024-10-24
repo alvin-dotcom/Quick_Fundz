e35c4b28cdca28e9c08a21d726422b8380e720b7
Rishav Raj <rajrishav011@gmail.com>
2024-10-24 21:10:16 +0530

    update auth folder

--- client/src/layout/auth/index.jsx
+++ client/src/layout/auth/index.jsx
@@ -2,21 +2,33 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const AuthLayout = () => {
-  const {isLoggedIn,role} = useSelector((state)=>state.auth)
-  
-  
-  if(isLoggedIn){
-    if(role === "admin"){
-return <Navigate to="/admin"/>;
-    }else if(role ==="user"){
-      return <Navigate to="/"/>
+  const { isLoggedIn, role, verificationStatus } = useSelector((state) => state.auth)
+
+
+  if (isLoggedIn) {
+    if (role === "admin"&& verificationStatus==="verified") {
+      return <Navigate to="/admin" />;
+    } else if (role === "user" && verificationStatus==="verified") {
+      return <Navigate to="/" />
     }
   }
 
+  /* if (role === "admin" && verificationStatus === "verified") {
+   return <Navigate to="/admin" />;
+ }
+ 
+ if (role === "user") {
+   if (verificationStatus === "verified") {
+     return <Navigate to="/" />;
+   } else {
+     return <Navigate to="/auth/kycstatus" />;
+   }
+ }  */
+
   return (
     <>
-    <Outlet />
-    
+      <Outlet />
+
     </>
   );
 };
