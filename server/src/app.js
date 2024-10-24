4d73e07c9735c3e0a8a02a6b40ddbbdaa7eae063
Rishav Raj <rajrishav011@gmail.com>
2024-10-24 21:21:30 +0530

    update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,7 @@ const helmet = require("helmet");
 const xss = require("xss-clean");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const cookieParser = require("cookie-parser");
 const app = express()
 
 app.use(cors({
@@ -18,10 +18,12 @@ app.use(cors({
 
 
 app.use(bodyParser.json());
-app.use(express.json({limit: "16kb"}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"))
 app.use(helmet());
+app.use(cookieParser())
 
 const limiter = rateLimit({
     max:3000,
