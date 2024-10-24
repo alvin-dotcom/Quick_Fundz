2f8483f17a37cf9e70b2d40eb0c63f136f5de01b
Rishav Raj <rajrishav011@gmail.com>
2024-10-24 21:08:27 +0530

    update admin folder

--- client/src/layout/admin/index.jsx
+++ client/src/layout/admin/index.jsx
@@ -1,19 +1,29 @@
-import { useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 import { useSelector } from "react-redux";
 
 
 const AdminLayout = () => {
-  const {isLoggedIn,role} = useSelector((state)=>state.auth)
+  const {isLoggedIn,role,verificationStatus} = useSelector((state)=>state.auth)
   
 
   
 
-  if(isLoggedIn) {
-    if(role === "user")
-      return <Navigate to="/"/>
-  }
+    if (isLoggedIn) {
+    // Ensure that role and verificationStatus exist before navigating
+    if (role === "user" && verificationStatus === "verified") {
+        return <Navigate to="/" />;
+      }
+    }
+  
+
+   /*    if (role === "user") {
+    if (verificationStatus === "verified") {
+      return <Navigate to="/" />;
+    } else {
+      return <Navigate to="/auth/kycstatus" />;
+    }
+  }  */
 
      if(!isLoggedIn){
     return <Navigate to="/auth/home"/>
