6d41a900fbd190a022cb88355969ae991fedec6a
Rishav Raj <rajrishav011@gmail.com>
2024-10-24 21:10:53 +0530

    update user folder

--- client/src/layout/user/index.jsx
+++ client/src/layout/user/index.jsx
@@ -4,10 +4,10 @@ import { useSelector } from "react-redux";
 
 
 const UserLayout = () => {
-  const {isLoggedIn,role} = useSelector((state)=>state.auth)
+  const {isLoggedIn,role,verificationStatus} = useSelector((state)=>state.auth)
   
   if(isLoggedIn){
-    if(role === "admin"){
+    if(role === "admin"&& verificationStatus==="verified"){
      return <Navigate to="/admin"/>;
    }
   }
