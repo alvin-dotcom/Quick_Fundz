4126d83aaafc028aacefabbf852ae9b3db9a1c5a
Rishav Raj <rajrishav011@gmail.com>
2024-10-24 21:13:02 +0530

    Update slices

--- client/src/redux/slices/auth.jsx
+++ client/src/redux/slices/auth.jsx
@@ -10,6 +10,7 @@ const initialState = {
   role: null,
   token: null,
   user_id: null,
+  verificationStatus:'pending',
 };
 
 const slice = createSlice({
@@ -24,6 +25,7 @@ const slice = createSlice({
       state.isLoggedIn = action.payload.isLoggedIn;
       state.token = action.payload.token;
       state.role = action.payload.role;
+      state.verificationStatus = action.payload.verificationStatus;
       state.user_id = action.payload.user_id;
       state.email = action.payload.email;
     },
@@ -37,6 +39,11 @@ const slice = createSlice({
     /* updateRegisterEmail(state, action) {
       state.email = action.payload.email;
     }, */
+    verifiedUser(state,action){
+      state.isLoggedIn = action.payload.isLoggedIn;
+      state.role=action.payload.role;
+      state.verificationStatus = action.payload.verificationStatus;
+    }
   },
 });
 
@@ -48,17 +55,30 @@ export function LoginUser(formValues) {
     try {
       const response = await axios.post("auth/login", formValues);
 
-      const { token, role, user_id, message, } = response.data;
-
-      dispatch(
-        slice.actions.logIn({
-          isLoggedIn: true,
-          role,
-          token,
-          user_id,
-          email: formValues.email,
-        })
-      );
+      const { token, role, user_id, message, verificationStatus} = response.data;
+      console.log(response.data)
+      if(verificationStatus === 'verified'){
+        dispatch(
+          slice.actions.logIn({
+            isLoggedIn: true,
+            role,
+            verificationStatus,
+            token,
+            user_id,
+            email: formValues.email,
+          })
+        );
+      }else{
+        dispatch(
+          slice.actions.logIn({
+            user_id,
+            verificationStatus
+          })
+        )
+        window.location.href='/auth/kycstatus'
+        
+      }
+      
 
       window.localStorage.setItem("user_id", user_id);
       //window.localStorage.setItem("token", token);
@@ -84,21 +104,22 @@ export function RegisterUser(formValues) {
   return async (dispatch, getState) => {
     let response;
     try {
-      response = await axios.post("auth/register", formValues);
+      response = await axios.post("auth/register", formValues,{withCredentials: true});
       const { user_id, message } = response.data;
 
 
       window.localStorage.setItem("user_id", user_id);
       //window.localStorage.setItem("token", token);
       toast.success(message || "Login Successful");
-
+if (!getState().auth.error) {
+        window.location.href = "/auth/kyc"
+}
     } catch (error) {
       toast.error(error.message || "Login failed.");
       //dispatch(slice.actions.updateIsLoading({ isLoading: false, error: true }));
-    } finally {
-      if (!getState().auth.error) {
-        window.location.href = "/auth/kyc"
-        const {token,role , user_id } = response.data;
+    } 
+      
+        /* const {token,role , user_id } = response.data;
       dispatch(
         slice.actions.logIn({
           isLoggedIn:true,
@@ -107,10 +128,70 @@ export function RegisterUser(formValues) {
           user_id,
           email: formValues.email,
         })
-      )
-
-      }
-
-    }
+      ) */
+      
   };
+  
 } 
+
+export function UserKyc(formValues){
+  return async(dispatch,getState)=>{
+    try {
+     const response =await axios.post("auth/kycEntry",formValues,{withCredentials: true})
+      const {message}=response.data;
+     toast.success(message || "KYC under processing ");
+
+     if(!getState().auth.error){
+      window.location.href = "/auth/kycstatus" 
+          }
+    } catch (error) {
+      toast.error(error.message)
+    }
+      
+  }
+}
+
+ export function verifiedKyc(formValues){
+  console.log(formValues);
+  return async(dispatch,getState)=>{
+    try {
+      const response= await axios.post('auth/verifiedKyc',formValues ,{withCredentials: true})
+      
+      const {role,message,verificationStatus}=response.data;
+      dispatch(
+        slice.actions.verifiedUser({
+          isLoggedIn:true,
+          role,
+          verificationStatus
+        })
+        
+      )
+      
+toast.success(message);
+    } catch (error) {
+      toast.error(error.message)
+    }
+  } 
+} 
+
+export function showKycRequest(){
+  return async (dispatch,getState)=>{
+    try {
+      const response = await axios.get('admin/showRequest')
+      return response.data;
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+}
+
+export function confirmOrRejectRequest(formValues){
+  return async(dispatch,getState)=>{
+    try {
+      const response = await axios.post('admin/confirmOrRejectUser',formValues);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+}
