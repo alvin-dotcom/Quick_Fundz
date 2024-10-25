4815e9038697ccdd8729fa79644b83a0c9679b0c
Rishav Raj <rajrishav011@gmail.com>
2024-10-25 22:23:17 +0530

    Add new route

--- server/src/routes/userKYC.js
+++ server/src/routes/userKYC.js
@@ -4,6 +4,6 @@ const authenticate = require('../middleware/authenticate.middleware');
 
 router.post('/kycEntry', authenticate,kycController.userKyc);
 router.post('/verifiedKyc',kycController.verifiedUser)
-
+router.post('/updatekyc',kycController.updateKyc)
 
 module.exports = router;
