44dbd89761f25bc38016ab7f0e3d2deabf85cf7f
Rishav Raj <rajrishav011@gmail.com>
2024-10-29 18:32:02 +0530

    Add new route path

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,11 +3,14 @@ const user = require('./user');
 const userKYC = require('./userKYC');
 const admin = require('./admin');
 const investment = require('./investmentRoutes'); 
-
+const loan = require('./loanRoutes')
+const negotiation = require('./negotiation')
 router.use("/auth", user);
 router.use("/auth", userKYC);
 router.use("/admin", admin);
 router.use("/investments", investment);
+router.use("/loanRequest",loan);
+router.use("/negotiateRequest",negotiation);
 
 
 module.exports = router;
