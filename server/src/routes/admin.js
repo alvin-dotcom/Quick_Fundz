67e4e1232a68909c33a6a8d10521d66869723a27
Rishav Raj <rajrishav011@gmail.com>
2024-10-29 18:31:37 +0530

    Update admin route

--- server/src/routes/admin.js
+++ server/src/routes/admin.js
@@ -3,7 +3,5 @@ const adminController = require('../controller/adminController')
 
 router.get('/showRequest',adminController.showKycRequest)
 router.post('/confirmOrRejectUser',adminController.confirm_OR_Reject_user)
-router.get('/users', adminController.getUsers);
-router.delete('/users/:role', adminController.deleteUser);
 
 module.exports = router;
