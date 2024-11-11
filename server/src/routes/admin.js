d563559316d2cf16f31ff640211c2c5e59368344
Rishav Raj <rajrishav011@gmail.com>
2024-11-11 23:47:21 +0530

    Add route in admin

--- server/src/routes/admin.js
+++ server/src/routes/admin.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
-const adminController = require('../controller/adminController')
+const adminController = require('../controller/adminController');
+const authenticate = require('../middleware/authenticate.middleware');
 
 router.get('/showRequest',adminController.showKycRequest)
 router.post('/confirmOrRejectUser',adminController.confirm_OR_Reject_user)
-router.get('/users', adminController.getAllKycDetails);
+router.get('/kycUsers', adminController.getAllKycDetails);
+router.get('/users', adminController.getAllUsers);
 router.delete('/deleteUser/:id', adminController.deleteUser);
+router.post('/adminUpdateKycUser', adminController.adminUpdateKycUser);
+router.post('/UpdateCurrent_user', adminController.adminUpdateUser);
+router.get('/message',authenticate, adminController.adminMessage);
+//router.get('/messageDelete',authenticate, adminController.adminMessageDelete);
 
 module.exports = router;
