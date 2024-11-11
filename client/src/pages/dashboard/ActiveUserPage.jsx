9dfb0d0c2de4ba63212e89fbc6a2f624e1ee22a2
Rishav Raj <rajrishav011@gmail.com>
2024-11-11 23:43:11 +0530

    Add activeUser page

---
+++ client/src/pages/dashboard/ActiveUserPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ActiveUsers from '../../components/admin/ActiveUsers'
+
+function ActiveUserPage() {
+  return (
+    <ActiveUsers/>
+  )
+}
+
+export default ActiveUserPage
