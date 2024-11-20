60599e3d746339f60a5712a4faaf279926c62dbd
Rishav Raj <rajrishav011@gmail.com>
2024-11-20 16:31:33 +0530

    Add new pages

---
+++ client/src/pages/dashboard/InvestedUsersPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import InvestedUsers from '../../components/admin/InvestedUsers'
+
+function InvestedUsersPage() {
+  return (
+    <>
+    <InvestedUsers/>
+    </>
+  )
+}
+
+export default InvestedUsersPage
---
+++ client/src/pages/dashboard/LoanRequestUsersPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LoanRequestUsers from '../../components/admin/LoanRequestUsers'
+
+function LoanRequestUsersPage() {
+  return (
+    <LoanRequestUsers/>
+  )
+}
+
+export default LoanRequestUsersPage
